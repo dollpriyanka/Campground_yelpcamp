3e6795ff914c5a560a655537d9d48214f9afb411
priyanka chatterjee <chatterjeepriyanka988@gmail.com>
2022-06-21 16:01:38 +0530

    complete

---
+++ cloudinary/index.js
@@ -0,0 +1,21 @@
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: 'YelpCamp',
+        allowedFormats: ['jpeg', 'png', 'jpg']
+    }
+});
+
+export default {
+    cloudinary,
+    storage
+}
